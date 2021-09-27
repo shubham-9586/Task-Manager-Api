b2266d346f42d066735a34a48294df93b3ddd89d
shubham kesharwani <shubhamkesharwani2356@gmail.com>
2021-09-27 12:08:54 +0530

    Added a Procfile.

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -16,13 +16,14 @@ router.post('/createuser',
   body('name').isLength({ min: 3 }), body('email').isEmail(), body('password').isLength({ min: 5 }),
   async (req, res) => {
     const errors = validationResult(req);
+    let success=false;
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ success,errors: errors.array() });
     }
     let user = await User.findOne({ email: req.body.email })
 
     if (user) {
-      return res.status(400).json({ error: "the user already exists" })
+      return res.status(400).json({success, error: "the user already exists" })
     }
     var salt = await bcrypt.genSaltSync(10);
     var secpass = await bcrypt.hashSync(req.body.password, salt);
@@ -37,7 +38,8 @@ router.post('/createuser',
       }
     }
     const jwtdata = jwt.sign(data, JWT_secret);
-    res.json({ jwtdata })
+    success=true;
+    res.json({success, jwtdata })
   })
 // ROUTE 2
 router.post('/login',
@@ -45,6 +47,7 @@ router.post('/login',
   body('password', 'password cant be blank').exists(),
   async (req, res) => {
     const errors = validationResult(req);
+  let success=false;
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
@@ -52,7 +55,7 @@ router.post('/login',
     try {
       let user = await User.findOne({ email })
       if (!user) {
-        return res.status(400).json({ error: "Enter correct credential" })
+        return res.status(400).json({ success,error: "Enter correct credential" })
       }
       let comparepassword = await bcrypt.compare(password, user.password)
 
@@ -65,8 +68,9 @@ router.post('/login',
           id: user.id
         }
       }
+      success=true;
       const jwtdata = jwt.sign(data, JWT_secret);
-      res.json({ jwtdata })
+      res.json({ success,jwtdata })
 
     } catch (error) {
       console.error(error.message)
--- backened/routes/notes.js
+++ backened/routes/notes.js
@@ -1,6 +1,4 @@
-const { getByTitle } = require('@testing-library/dom');
 const express=require('express')
-
 const router=express.Router();
 
 const { body, validationResult } = require('express-validator');
@@ -9,11 +7,10 @@ const fetchuser=require('../middleware/fetchuser')
 
 const Notes=require('../models/Notes')
 
-router.get('./fetchalluser', fetchuser, async(req,res)=>{
-
+router.get('/fetchalluser', fetchuser, async(req,res)=>{
     try {
         
-        const notes=await notes.find({user:req.user.id});
+        const notes=await Notes.find({user:req.user.id});
         res.json(notes);
     } catch (error) {
         console.error(error.message)
@@ -21,8 +18,7 @@ router.get('./fetchalluser', fetchuser, async(req,res)=>{
     }
 
 })
-
-router.post('./addnote', fetchuser,  [
+router.post('/addnotes', fetchuser,  [
     body('title').isLength({ min: 3 }),
   body('description').isLength({ min: 5 })],
 async(req,res)=>{
@@ -32,12 +28,11 @@ async(req,res)=>{
     }
     try {
     const {title ,description,tag}=req.body
-        const notes=new Notes({
+        const notes=await new Notes({
         title,description,tag,user:req.user.id
         })
         const saveNote=await notes.save();
         res.json(saveNote)
-        res.send("500")
     } catch (error) {
         console.error(error.message)
         res.status(500).send("error occured");
@@ -45,5 +40,54 @@ async(req,res)=>{
     
 })
 
+router.put('/update/:id', fetchuser,  
+async(req,res)=>{
+  console.log(req.params.id);
+  try {
+  const {title ,description,tag}=req.body
+  const newnote={};
+      if(title){newnote.title=title}
+      if(description){newnote.description=description}
+      if(tag){newnote.tag=tag}
+      let note=await Notes.findById(req.params.id);
+      if(!note)
+      {
+        return res.status(404).send("Result not found")
+      }
+      
+      if(note.user.toString()!==req.user.id)
+      {
+        return res.status(401).send("Invalid Token")
+      }
+      
+      note= await Notes.findByIdAndUpdate(req.params.id,{$set:newnote},{new:true})
+      res.json(note)
 
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("error occured");
+  }
+})
+
+  router.delete('/deletenote/:id', fetchuser,  
+async(req,res)=>{
+  try {
+      let note=await Notes.findById(req.params.id);
+      if(!note)
+      {
+        return res.status(404).send("Result not found")
+      }
+      if(note.user.toString()!==req.user.id)
+      {
+        return res.status(401).send("Invalid Token")
+      }
+      
+     note= await Notes.findByIdAndDelete(req.params.id);
+     res.status(200).send("Deleted successfully")
+
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("error occured");
+  }
+})
 module.exports=router
